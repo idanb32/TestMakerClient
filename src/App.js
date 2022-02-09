39cab92cdd5f06d60cc8c93351ffe3c5190964c8
unknown <bsl3232@gmail.com>
2022-02-09 13:43:11 +0200

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+const App = ()=> {
+  return (
+    <div className="App">
+     in app im sane yay
+     <Router>
+
+     </Router>
+    </div>
+  );
+}
+
+export default App;
