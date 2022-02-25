55d968e761f0b52409b9be3bd2c94be622391f8e
idanb32 <bsl3232@gmail.com>
2022-02-25 11:42:19 +0200

    comit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ import ClientTest from './Views/ClientTest/ClientTest';
 import ClientFinishedTest from './Views/ClientFinishedTest/ClientFinishedTest';
 import EditQuestion from './Views/EditQuestion/EditQuestiom';
 import ClientMenu from './Views/ClientMenu/ClientMenu';
-
+import EditQuiz from './Views/EditQuiz/EditQuiz';
+import SearchReport from './Views/SearchReport/SearchReport';
+import TestReport from './Views/TestReport/TestReport';
 
 const App = () => {
   
@@ -31,9 +33,12 @@ const App = () => {
           <Route path='/QuizMenu' element={<MangeQuiz />}> </Route>
           <Route path='/QuestionMenu'  element={<MangeQuestion />} ></Route>
           <Route path='/EditQuestion'  element={<EditQuestion />} ></Route>
+          <Route path='/EditQuiz'  element={<EditQuiz />} ></Route>
           <Route path='/ClientTest'  element={<ClientTest  />} ></Route>
           <Route path='/FinishedTest'  element={<ClientFinishedTest />} ></Route>
           <Route path='/ClientMenu'  element={<ClientMenu userId="620baa6d15ea5a324d801a8f"/>} ></Route>
+          <Route path='/SearchReport'  element={<SearchReport  />} ></Route>
+          <Route path='/TestReport'  element={<TestReport  />} ></Route>
         </Routes>
       </BrowserRouter>
     </div>
