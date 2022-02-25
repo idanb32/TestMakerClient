749f4b070d8d4ccd38caf3dd2b8f61ddcec65699
idanb32 <bsl3232@gmail.com>
2022-02-25 14:30:33 +0200

    comit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<LoginPage />}>
+          <Route path="/" element={<LoginPage />}>
           
           </Route>
           <Route path="/MainMenu" element={<MainMenu />}>
